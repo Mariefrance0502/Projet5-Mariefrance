1c08918ef63f961b7cc9d58ceeed081b8b98f081
Marie <roselin_marie@yahoo.fr>
2023-05-07 15:17:25 +0200

    mod

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,17 @@
-console.log("hello")
+
+// Création de l'url pour recupérer les donnnés de l'API 
+const url = "http://localhost:3000/api/products/"
+
+const container = document.getElementById("items")
+
+
+const getArticles = () => { 
+   fetch (url)
+  .then (function (res) {
+      return res.json ()
+})
+  .then(function (data) { 
+
+})
+}
+getArticles()
