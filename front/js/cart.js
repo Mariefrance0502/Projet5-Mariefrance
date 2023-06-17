1abc2bfb579ad031e2bdca8bb0cc880b69a73712
Marie <roselin_marie@yahoo.fr>
2023-06-17 23:30:05 +0200

    Modification fichier product

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,2 +1,87 @@
-var localStorageObject = localStorage.getItem('browsers');
-console.log(JSON.parse(localStorageObject));
+
+
+// RECUPERER LES PRODUITS STOCKES DANS LE LOCALSTORAGE   //
+let addToLocalStorage = JSON.parse(localStorage.getItem('addToCart'));
+console.log(addToLocalStorage); 
+
+// AFFICHER LES PRODUITS DU PANIER
+
+ // je sélectionne la partie html concernée par la modification
+ let cartAndFormContainer = document.getElementById('cartAndFormContainer');
+ console.log(cartAndFormContainer); 
+
+ // si le panier est vide : afficher 'le panier est vide'
+if(addToLocalStorage === null || addToLocalStorage == 0) {
+  document.querySelector("#cart__items").innerHTML =`
+  <div class="cart__empty">
+    <h2>Votre panier est vide ! <br> Merci de sélectionner des produits depuis la page d'accueil</h2>
+  </div>`;
+}
+
+/*
+
+// si le panier n'est pas vide : afficher les produits dans le localStorage
+else{
+  let itemCards = [];
+
+  // expression initiale; condition; incrémentation
+  for (i = 0; i < addToLocalStorage.length; i++) {
+  products.push(addToLocalStorage[i].id);
+
+  // le code suivant sera injecté à chaque tour de boucle
+  // selon la longueur des produits dans le local storage
+  itemCards = itemCards + `
+    
+    <article class="cart__item" data-id="${addToLocalStorage[i].ProductId}" data-color="${addToLocalStorage.color}">
+    <div class="cart__item__img">
+      <img src="${addToLocalStorage[i].products.imageUrl}}" alt="${addToLocalStorage[i].alt}">
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__titlePrice">
+        <h2>${addToLocalStorage[i].name}</h2>
+        <p>${addToLocalStorage[i].color}</p>
+
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${addToLocalStorage[i].quantity}">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+      </div>
+    </div>
+  </article>
+    `;
+  }
+  if (i === addToLocalStorage.length) {
+  const itemCart = document.getElementById('cart__items');
+  itemCart.innerHTML += itemCards;
+  }
+
+
+  // je modifie la quantité dans le panier
+function changeQtt() {
+  let itemQtt = document.querySelectorAll('.itemQuantity');
+  for (let j = 0; j < itemQtt.length; j++) {
+    itemQtt[j].addEventListener('change', (event) => {
+    event.preventDefault();
+    // sélection de la nouvelle quantité...
+    // ... qu'on va sauvegarder dans un nouveau tableau
+   // avec les autres éléments du localStorage
+    let itemNewQtt = itemQtt[j].value;
+      const newLocalStorage = {
+      id: addToLocalStorage[j].id,
+      image: addToLocalStorage[j].image,
+      alt: addToLocalStorage[j].alt,
+      name: addToLocalStoragee[j].name,
+      color: addToLocalStoragee[j].color,
+      price: productInLocalStorage[j].price,   
+      quantity: itemNewQtt, // avec la nouvelle quantité souhaitée
+   
+    };
+  })
+  }}}
+
+ */
--- front/js/product.js
+++ front/js/product.js
@@ -3,6 +3,8 @@ const searchParams = new URL(window.location).searchParams;
 const id = searchParams.get('id');
 const url = `http://localhost:3000/api/products/${id}`
 
+
+//Cette fonction permet de récupérer les informations du produit depuis l'Api du backend en fonction de l'identifiant
 const getArticles = () => { 
       fetch(url)
       .then (function (res) { 
@@ -24,22 +26,22 @@ const getArticles = () => {
       })
       
 }
-   
-
-
-
- // Fonction déclenchée au clic sur le bouton addtocart
+  
+    // Fonction déclenchée au clic sur le bouton addtocart
 
     const Colors = document.getElementById("colors")
     const quantity= document.getElementById("quantity")
-    // on click, save, add, product selected by ID in local Storage //
     const addToCart = document.getElementById("addToCart")
+
+
+    
     addToCart.addEventListener("click", () => {
     const quantitySelected = parseInt (quantity.value); //  ParesInt = VALEUR ENTIER 
     const colorsSelected = Colors.value; 
     const idSelected = id;
-  if ( colorsSelected !== ""  & quantitySelected> 0 & quantitySelected <= 100) 
-  { 
+
+
+  if ( colorsSelected !== ""  & quantitySelected> 0 & quantitySelected <= 100){ 
     // Préparation des données pour la panier + variable sous forme d'objet 
     // display in local storage all the data we need to make an order //
     let Kanap =
@@ -49,48 +51,51 @@ const getArticles = () => {
         color : document.getElementById('colors').value
     }
     alert("Le produit a bien été ajoutée au panier")
- }
- 
-    else {
-      // affichage des messages d'erreur 
+     
+//Initialisation du local storage, vérifier si les Local Storage est vide 
+let addToLocalStorage = JSON.parse(localStorage.getItem("addToCart")); 
 
-      if ( colorsSelected === "" || quantitySelected <= 0 & quantitySelected > 100) {
-       alert ("Veillez choisir une couleur et une quantitée")
-      }
-      else if ( quantitySelected == 0) {
-       alert ("Veillez choisir une quantitée correcte comprise entre 1 et 100")
-      }
-     // Il manque le messaged'erreur pour la couleur !!! 
-     else if (colorsSelected == "") {
-      alert ("Veillez choisir une couleur") // Ne fonctionne pas 
-     }
-      }    
 
-addToLocalStorage = []
+// Le Locale Storage est vide 
+addToLocalStorage = []; 
+addToLocalStorage.push(Kanap); 
+localStorage.setItem("addToCart", JSON.stringify(addToLocalStorage));
 console.log(addToLocalStorage);
 
-if (localStorage.getItem("addToCart")){
-// Si il y a deja des produit dans le locale storage :
-  let item = localStorageProducts.find(
-    (item) =>
-    item.id == Kanap.id && item.colors == Kanap.colors
-  ); 
-    // Si oui on additionne les quantity des articles 
-    if (item) {
-      item.quantity = item.quantity + Kanap.quantity;
-      localStorage.setItem("addToCart", JSON.stringify(addToLocalStorage));
-      return;
-  }
-
-} else { 
-  localStorage.setItem("addToCart", JSON.stringify(addToLocalStorage))
-  addToLocalStorage.push(addToCart)
+// Si le panier comporte déja au moins un produit 
+  // Verifier si le produit est la même couleur et le même id
+if (addToLocalStorage !== "" ){
+  let item = addToLocalStorage.find ((item) => item.id === Kanap.id && item.color === Kanap.color); 
+  console.log('###### item', item);
+  if (item) {
+    let newQuantity = item.quantity + Kanap.quantity; 
+    item.quantity = newQuantity; 
+    console.log('######  item ',newQuantity); 
+    localStorage.setItem("addToCart", JSON.stringify(addToLocalStorage));
+  } else{
+      // Le produit selectionné n'est pas dans la panier ou dans le local storage 
+         addToLocalStorage.push(Kanap); 
+         localStorage.setItem("addToCart", JSON.stringify(addToLocalStorage)); 
+      }
 }
-console.log(addToLocalStorage); 
 
+} 
+else {
+  // affichage des messages d'erreur 
 
-})
+  if ( colorsSelected === "" || quantitySelected <= 0 & quantitySelected > 100) {
+   alert ("Veillez choisir une couleur et une quantitée")
+  }
+  else if ( quantitySelected == 0) {
+   alert ("Veillez choisir une quantitée correcte comprise entre 1 et 100")
+  }
+ // Il manque le messaged'erreur pour la couleur !!! 
+ else if (colorsSelected == "") {
+  alert ("Veillez choisir une couleur") // Ne fonctionne pas 
+ }
+  }  
+}   
+) 
 
 getArticles ()
-
 
